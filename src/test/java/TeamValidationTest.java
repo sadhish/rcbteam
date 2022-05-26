5aa76c128c5e3181a1494737d4fe1cd68ea6b832
sadhish <sadhishjay@gmail.com>
2022-05-26 15:55:43 +0530

    Testcases to validate foreign player and wicket keeper as per the given req

---
+++ src/test/java/TeamValidationTest.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class TeamValidationTest extends CommonUtils {
+
+    @Test(description = "test validates that team has equal or less than four foreign players")
+
+    void testForeignPlayersCount() throws IOException {
+        boolean foreignPlayerCount = dataRequest().getPlayer()
+                .stream()
+                .filter(e -> !e.get("country").equalsIgnoreCase("India"))
+                .count() <=4?true:false;
+        Assert.assertEquals(true, foreignPlayerCount, "Foreign Players should be less than or equal to 4");
+
+
+    }
+
+    @Test(description = "test validates that team has atleast one wicket keeper")
+    void testAtleastOneWicketKeeperExist() throws IOException {
+        boolean wicketKeeperCount = dataRequest().getPlayer()
+                .stream()
+                .filter(e -> e.get("role").equalsIgnoreCase("Wicket-keeper"))
+                .count() > 0 ? true : false;
+        Assert.assertEquals(true, wicketKeeperCount, "One Wicket Keeper should be in the Team");
+    }
+
+}
