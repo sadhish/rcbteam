39ba6347f8778075fed2452a612b7604b60a9cd9
sadhish <sadhishjay@gmail.com>
2022-05-26 15:37:47 +0530

    Added test-data Json file inside resources folder and created a pojo class for it

---
+++ src/test/java/datatranserobject/RcbPojo.java
@@ -0,0 +1,27 @@
+package datatranserobject;
+import java.util.List;
+import java.util.Map;
+public class RcbPojo {
+    private String name;
+    private String location;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private List<Map<String,String>> player;
+
+    public List<Map<String, String>> getPlayer() {
+        return player;
+    }
+    public void setPlayer(List<Map<String, String>> player) {
+        this.player = player;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
